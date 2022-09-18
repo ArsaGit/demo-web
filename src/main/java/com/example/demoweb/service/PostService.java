45efedbe18b09e12ceb07092692e5898f2eda891
Artyom HuaweiMatebook <artyom.arsalanov@gmail.com>
2022-09-19 04:16:34 +0500

    9.1-9.2

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -1,6 +1,8 @@
 package com.example.demoweb.service;
 
 import com.example.demoweb.model.Post;
+import com.example.demoweb.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.text.DateFormat;
@@ -12,19 +14,27 @@ import java.util.function.LongConsumer;
 @Service
 public class PostService {
     private List<Post> posts;
+    @Autowired
+    PostRepository postRepository;
 
     public PostService(){
         posts = new ArrayList<>();
-        create("First post");
-        create("Second post");
-        create("Third post");
+        posts.add(new Post(0L,"пост 1", new Date()));
+        posts.add(new Post(1L,"пост 2", new Date()));
+        posts.add(new Post(2L,"пост 3", new Date()));
     }
 
-    public List<Post> listAllPosts(){
+    /*public List<Post> listAllPosts() {
         return posts;
+    }*/
+    public Iterable<Post> listAllPosts() {
+        return postRepository.findAll();
     }
 
     public void create(String text) {
-        posts.add(new Post( Long.valueOf(posts.size()), text, new Date()));
+        long newId = posts.size();
+        posts.add(new Post(newId, text, new Date()));
+        Post post = new Post(null, text, new Date());
+        postRepository.save(post);
     }
 }
