0e9dc158648be07e54373d09ddd06e8d3d73db1f
Artyom HuaweiMatebook <artyom.arsalanov@gmail.com>
2022-10-03 02:13:15 +0500

    step6

--- src/main/java/com/example/demoweb/servlet/MainServlet.java
+++ src/main/java/com/example/demoweb/servlet/MainServlet.java
@@ -1,12 +1,13 @@
 package com.example.demoweb.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
+@WebServlet("/")
 public class MainServlet extends HttpServlet{
 
     @Override
